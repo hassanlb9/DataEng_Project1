283747abef15f8ca918304117c8115ef5f6056d0
alou28 <jean.alain.ngomedj@efrei.net>
2021-12-12 19:20:58 +0100

    model add to flask app

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from flask import Flask, render_template, request
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
 import unittest
-nltk.download('vader_lexicon')
+import pickle
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+import keras
+
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def main():
+config = pickle.load(open('./config.pkl', 'rb'))
+model = keras.models.load_model('./model_sentiment_v1.h5')
+
+
+@app.route('/senti', methods=["GET", "POST"])
+def predict_sentiment():
+    config = pickle.load(open('./config.pkl', 'rb'))
+    model = keras.models.load_model('./model_sentiment_v1.h5')
     if request.method == "POST":
-        inp = request.form.get("inp")
-        sid = SentimentIntensityAnalyzer()
-        score = sid.polarity_scores(inp)
-        if score["neg"] != 0:
-            return render_template('home.html', message="Negative")
-        else:
-            return render_template('home.html', message="Positive")
-    return render_template('home.html')
+       inp = request.form.get("inp")
+       text = [inp] if type(inp) == np.str else inp
+       tokenizer = config['tokenizer']['tokenizer']
+       text = tokenizer.texts_to_sequences(text)
+       text = pad_sequences(text, maxlen     = config['tokenizer']['token_maxlen'],
+                               padding    = config['tokenizer']['padding_type'],
+                               truncating = config['tokenizer']['truncating_type'],
+                               value      = config['tokenizer']['padding_value'])
+       sentiment = model.predict(text,batch_size=1,verbose = 0)[0]
+       argmax_sent = np.argmax(sentiment)
+       if  argmax_sent == 1:
+           return render_template('home.html', message="Positive")
+       if  argmax_sent == 0:
+           return render_template('home.html', message="Neutre")
+       if  argmax_sent != 1:
+           return render_template('home.html', message="Negative")
+       
+       
+       #sentiment_text  = 'Positive' if  argmax_sent == 1 ifElse 'Negative'
+       #sentiment_score = sentiment[argmax_sent]
+    #return((sentiment_text, sentiment_score)) 
+    return render_template('home.html')    
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host = "127.0.0.1", port = "8000")
