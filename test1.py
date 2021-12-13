0d172de0682809896a4f9b876348f957aa613788
alou28 <93199929+alou28@users.noreply.github.com>
2021-12-13 23:18:17 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,18 @@
+import unittest
+from main1 import app
+
+
+
+class site_test(unittest.TestCase):
+   def test_home(self):
+       tester=app.test_client(self)
+       response = tester.get("/sent")
+       statuscode=response.status_code
+       self.assertEqual(statuscode,200)
+       
+
+     
+if __name__ == "__main__":
+    unittest.main()
+
+
